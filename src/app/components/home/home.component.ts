5219e2df515cc0c4dd56d1d1273906c5620ff577
mucahit.kamat <mucahit.kamat@dgpays.com>
2022-12-15 01:26:20 +0300

    template up

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  styleClass = '';
+  constructor() {}
+  ngOnInit(): void {}
 
+  changeStyleClass(text: string) {
+    return 'fixed-plugin ps ' + text;
+  }
 }
